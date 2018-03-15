3e05c3891b7537d87204f72ad4bd258acdf173e0
Vladimir Iglovikov <viglovikov@lyft.com>
2018-03-15 16:51:27 -0700

    bugfix

--- unet_models.py
+++ unet_models.py
@@ -4,6 +4,7 @@ import torch
 from torchvision import models
 import torchvision
 
+
 def conv3x3(in_, out):
     return nn.Conv2d(in_, out, 3, padding=1)
 
@@ -139,9 +140,10 @@ class DecoderBlockV2(nn.Module):
 
 class TernausNet34(nn.Module):
     """
-    UNet (https://arxiv.org/abs/1505.04597) with Resnet34(https://arxiv.org/abs/1512.03385) encoder
+        UNet (https://arxiv.org/abs/1505.04597) with Resnet34(https://arxiv.org/abs/1512.03385) encoder
 
-    """
+        """
+
     def __init__(self, num_classes=1, num_filters=32, pretrained=False, is_deconv=False):
         """
         :param num_classes:
@@ -167,7 +169,8 @@ class TernausNet34(nn.Module):
 
         self.conv1 = nn.Sequential(self.encoder.conv1,
                                    self.encoder.bn1,
-                                   self.encoder.relu)
+                                   self.encoder.relu,
+                                   self.pool)
 
         self.conv2 = self.encoder.layer1
 
@@ -180,18 +183,19 @@ class TernausNet34(nn.Module):
         self.center = DecoderBlockV2(512, num_filters * 8 * 2, num_filters * 8, is_deconv)
 
         self.dec5 = DecoderBlockV2(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec4 = DecoderBlockV2(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec3 = DecoderBlockV2(256 + num_filters * 8, num_filters * 4 * 2, num_filters * 2, is_deconv)
-        self.dec2 = DecoderBlockV2(128 + num_filters * 2, num_filters * 2 * 2, num_filters, is_deconv)
-        self.dec1 = ConvRelu(64 + num_filters, num_filters)
+        self.dec4 = DecoderBlockV2(256 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
+        self.dec3 = DecoderBlockV2(128 + num_filters * 8, num_filters * 4 * 2, num_filters * 2, is_deconv)
+        self.dec2 = DecoderBlockV2(64 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2, is_deconv)
+        self.dec1 = DecoderBlockV2(num_filters * 2 * 2, num_filters * 2 * 2, num_filters, is_deconv)
+        self.dec0 = ConvRelu(num_filters, num_filters)
         self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
 
     def forward(self, x):
         conv1 = self.conv1(x)
-        conv2 = self.conv2(self.pool(conv1))
-        conv3 = self.conv3(self.pool(conv2))
-        conv4 = self.conv4(self.pool(conv3))
-        conv5 = self.conv5(self.pool(conv4))
+        conv2 = self.conv2(conv1)
+        conv3 = self.conv3(conv2)
+        conv4 = self.conv4(conv3)
+        conv5 = self.conv5(conv4)
 
         center = self.center(self.pool(conv5))
 
@@ -200,11 +204,12 @@ class TernausNet34(nn.Module):
         dec4 = self.dec4(torch.cat([dec5, conv4], 1))
         dec3 = self.dec3(torch.cat([dec4, conv3], 1))
         dec2 = self.dec2(torch.cat([dec3, conv2], 1))
-        dec1 = self.dec1(torch.cat([dec2, conv1], 1))
+        dec1 = self.dec1(dec2)
+        dec0 = self.dec0(dec1)
 
         if self.num_classes > 1:
-            x_out = F.log_softmax(self.final(dec1), dim=1)
+            x_out = F.log_softmax(self.final(dec0), dim=1)
         else:
-            x_out = self.final(dec1)
+            x_out = self.final(dec0)
 
         return x_out
