14f4b469f1ddc3acde2374e8161f4a1b8173cd35
Vladimir Iglovikov <iglovikov@gmail.com>
2020-05-28 12:42:22 -0700

    fixes

---
+++ setup.py
@@ -0,0 +1,112 @@
+import io
+import os
+import sys
+from shutil import rmtree
+import re
+from setuptools import Command, find_packages, setup
+
+# Package meta-data.
+name = "ternausnet"
+description = "UNet with VGG encoders"
+url = "https://github.com/ternaus/TernausNet"
+email = "iglovikov@gmail.com"
+author = "Vladimir Iglovikov"
+requires_python = ">=3.0.0"
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def get_version():
+    version_file = os.path.join(current_dir, name, "__init__.py")
+    with io.open(version_file, encoding="utf-8") as f:
+        return re.search(r'^__version__ = [\'"]([^\'"]*)[\'"]', f.read(), re.M).group(1)
+
+
+# What packages are required for this module to be executed?
+try:
+    with open(os.path.join(current_dir, "requirements.txt"), encoding="utf-8") as f:
+        required = f.read().split("\n")
+except FileNotFoundError:
+    required = []
+
+# What packages are optional?
+extras = {"test": ["pytest"]}
+
+version = get_version()
+
+about = {"__version__": version}
+
+
+def get_test_requirements():
+    requirements = ["pytest"]
+    if sys.version_info < (3, 3):
+        requirements.append("mock")
+    return requirements
+
+
+def get_long_description():
+    base_dir = os.path.abspath(os.path.dirname(__file__))
+    with io.open(os.path.join(base_dir, "README.md"), encoding="utf-8") as f:
+        return f.read()
+
+
+class UploadCommand(Command):
+    """Support setup.py upload."""
+
+    description = "Build and publish the package."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Print things in bold."""
+        print(s)
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        try:
+            self.status("Removing previous builds...")
+            rmtree(os.path.join(current_dir, "dist"))
+        except OSError:
+            pass
+
+        self.status("Building Source and Wheel (universal) distribution...")
+        os.system("{0} setup.py sdist bdist_wheel --universal".format(sys.executable))
+
+        self.status("Uploading the package to PyPI via Twine...")
+        os.system("twine upload dist/*")
+
+        self.status("Pushing git tags...")
+        os.system("git tag v{0}".format(about["__version__"]))
+        os.system("git push --tags")
+
+        sys.exit()
+
+
+setup(
+    name=name,
+    version=version,
+    description=description,
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Vladimir Iglovikov",
+    license="MIT",
+    url=url,
+    packages=find_packages(exclude=["tests", "docs", "images"]),
+    install_requires=required,
+    extras_require=extras,
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Science/Research",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+    ],
+    cmdclass={"upload": UploadCommand},
+)
--- ternausnet/models.py
+++ ternausnet/models.py
@@ -1,28 +1,28 @@
+import torch
+import torchvision
 from torch import nn
 from torch.nn import functional as F
-import torch
 from torchvision import models
-import torchvision
 
 
-def conv3x3(in_, out):
+def conv3x3(in_: int, out: int) -> nn.Module:
     return nn.Conv2d(in_, out, 3, padding=1)
 
 
 class ConvRelu(nn.Module):
-    def __init__(self, in_, out):
+    def __init__(self, in_: int, out: int) -> None:
         super().__init__()
         self.conv = conv3x3(in_, out)
         self.activation = nn.ReLU(inplace=True)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.conv(x)
         x = self.activation(x)
         return x
 
 
 class DecoderBlock(nn.Module):
-    def __init__(self, in_channels, middle_channels, out_channels):
+    def __init__(self, in_channels: int, middle_channels: int, out_channels: int) -> None:
         super().__init__()
 
         self.block = nn.Sequential(
@@ -31,18 +31,19 @@ class DecoderBlock(nn.Module):
             nn.ReLU(inplace=True)
         )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.block(x)
 
 
 class UNet11(nn.Module):
-    def __init__(self, num_filters=32, pretrained=False):
+    def __init__(self, num_filters: int = 32, pretrained: bool = False) -> None:
         """
-        :param num_classes:
-        :param num_filters:
-        :param pretrained:
-            False - no pre-trained network is used
-            True  - encoder is pre-trained with VGG11
+
+        Args:
+            num_filters:
+            pretrained:
+                False - no pre-trained network is used
+                True  - encoder is pre-trained with VGG11
         """
         super().__init__()
         self.pool = nn.MaxPool2d(2, 2)
@@ -68,7 +69,7 @@ class UNet11(nn.Module):
 
         self.final = nn.Conv2d(num_filters, 1, kernel_size=1)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         conv1 = self.relu(self.conv1(x))
         conv2 = self.relu(self.conv2(self.pool(conv1)))
         conv3s = self.relu(self.conv3s(self.pool(conv2)))
@@ -88,39 +89,23 @@ class UNet11(nn.Module):
         return self.final(dec1)
 
 
-def unet11(pretrained=False, **kwargs):
-    """
-    pretrained:
-            False - no pre-trained network is used
-            True  - encoder is pre-trained with VGG11
-            carvana - all weights are pre-trained on
-                Kaggle: Carvana dataset https://www.kaggle.com/c/carvana-image-masking-challenge
-    """
-    model = UNet11(pretrained=pretrained, **kwargs)
-
-    if pretrained == 'carvana':
-        state = torch.load('TernausNet.pt')
-        model.load_state_dict(state['model'])
-    return model
-
-
 class Interpolate(nn.Module):
-    def __init__(self, size=None, scale_factor=None, mode='nearest', align_corners=False):
+    def __init__(self, size: int = None, scale_factor: int = None, mode: str = 'nearest', align_corners: bool = False):
         super(Interpolate, self).__init__()
         self.interp = nn.functional.interpolate
         self.size = size
         self.mode = mode
         self.scale_factor = scale_factor
         self.align_corners = align_corners
-        
-    def forward(self, x):
-        x = self.interp(x, size=self.size, scale_factor=self.scale_factor, 
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        x = self.interp(x, size=self.size, scale_factor=self.scale_factor,
                         mode=self.mode, align_corners=self.align_corners)
         return x
 
 
 class DecoderBlockV2(nn.Module):
-    def __init__(self, in_channels, middle_channels, out_channels, is_deconv=True):
+    def __init__(self, in_channels: int, middle_channels: int, out_channels: int, is_deconv: bool=True):
         super(DecoderBlockV2, self).__init__()
         self.in_channels = in_channels
 
@@ -143,97 +128,23 @@ class DecoderBlockV2(nn.Module):
                 ConvRelu(middle_channels, out_channels),
             )
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         return self.block(x)
 
 
-class AlbuNet(nn.Module):
-    """
-        UNet (https://arxiv.org/abs/1505.04597) with Resnet34(https://arxiv.org/abs/1512.03385) encoder
-
-        Proposed by Alexander Buslaev: https://www.linkedin.com/in/al-buslaev/
-
-        """
-
-    def __init__(self, num_classes=1, num_filters=32, pretrained=False, is_deconv=False):
-        """
-        :param num_classes:
-        :param num_filters:
-        :param pretrained:
-            False - no pre-trained network is used
-            True  - encoder is pre-trained with resnet34
-        :is_deconv:
-            False: bilinear interpolation is used in decoder
-            True: deconvolution is used in decoder
-        """
-        super().__init__()
-        self.num_classes = num_classes
-
-        self.pool = nn.MaxPool2d(2, 2)
-
-        self.encoder = torchvision.models.resnet34(pretrained=pretrained)
-
-        self.relu = nn.ReLU(inplace=True)
-
-        self.conv1 = nn.Sequential(self.encoder.conv1,
-                                   self.encoder.bn1,
-                                   self.encoder.relu,
-                                   self.pool)
-
-        self.conv2 = self.encoder.layer1
-
-        self.conv3 = self.encoder.layer2
-
-        self.conv4 = self.encoder.layer3
-
-        self.conv5 = self.encoder.layer4
-
-        self.center = DecoderBlockV2(512, num_filters * 8 * 2, num_filters * 8, is_deconv)
-
-        self.dec5 = DecoderBlockV2(512 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec4 = DecoderBlockV2(256 + num_filters * 8, num_filters * 8 * 2, num_filters * 8, is_deconv)
-        self.dec3 = DecoderBlockV2(128 + num_filters * 8, num_filters * 4 * 2, num_filters * 2, is_deconv)
-        self.dec2 = DecoderBlockV2(64 + num_filters * 2, num_filters * 2 * 2, num_filters * 2 * 2, is_deconv)
-        self.dec1 = DecoderBlockV2(num_filters * 2 * 2, num_filters * 2 * 2, num_filters, is_deconv)
-        self.dec0 = ConvRelu(num_filters, num_filters)
-        self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
-
-    def forward(self, x):
-        conv1 = self.conv1(x)
-        conv2 = self.conv2(conv1)
-        conv3 = self.conv3(conv2)
-        conv4 = self.conv4(conv3)
-        conv5 = self.conv5(conv4)
-
-        center = self.center(self.pool(conv5))
-
-        dec5 = self.dec5(torch.cat([center, conv5], 1))
-
-        dec4 = self.dec4(torch.cat([dec5, conv4], 1))
-        dec3 = self.dec3(torch.cat([dec4, conv3], 1))
-        dec2 = self.dec2(torch.cat([dec3, conv2], 1))
-        dec1 = self.dec1(dec2)
-        dec0 = self.dec0(dec1)
-
-        if self.num_classes > 1:
-            x_out = F.log_softmax(self.final(dec0), dim=1)
-        else:
-            x_out = self.final(dec0)
-
-        return x_out
-
-
 class UNet16(nn.Module):
-    def __init__(self, num_classes=1, num_filters=32, pretrained=False, is_deconv=False):
+    def __init__(self, num_classes: int = 1, num_filters: int = 32, pretrained: bool = False, is_deconv: bool = False):
         """
-        :param num_classes:
-        :param num_filters:
-        :param pretrained:
-            False - no pre-trained network used
-            True - encoder pre-trained with VGG16
-        :is_deconv:
-            False: bilinear interpolation is used in decoder
-            True: deconvolution is used in decoder
+
+        Args:
+            num_classes:
+            num_filters:
+            pretrained:
+                False - no pre-trained network used
+                True - encoder pre-trained with VGG16
+            is_deconv:
+                False: bilinear interpolation is used in decoder
+                True: deconvolution is used in decoder
         """
         super().__init__()
         self.num_classes = num_classes
@@ -284,7 +195,7 @@ class UNet16(nn.Module):
         self.dec1 = ConvRelu(64 + num_filters, num_filters)
         self.final = nn.Conv2d(num_filters, num_classes, kernel_size=1)
 
-    def forward(self, x):
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
         conv1 = self.conv1(x)
         conv2 = self.conv2(self.pool(conv1))
         conv3 = self.conv3(self.pool(conv2))
@@ -299,10 +210,4 @@ class UNet16(nn.Module):
         dec3 = self.dec3(torch.cat([dec4, conv3], 1))
         dec2 = self.dec2(torch.cat([dec3, conv2], 1))
         dec1 = self.dec1(torch.cat([dec2, conv1], 1))
-
-        if self.num_classes > 1:
-            x_out = F.log_softmax(self.final(dec1), dim=1)
-        else:
-            x_out = self.final(dec1)
-
-        return x_out
+        return self.final(dec1)
